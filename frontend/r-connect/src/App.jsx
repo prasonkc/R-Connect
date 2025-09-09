80cc700be4383659b86972143b1133cffaafb32a
Prason <prasonkc07@gmail.com>
2025-09-09 18:40:21 +0545

    Skeleton

--- frontend/r-connect/src/App.jsx
+++ frontend/r-connect/src/App.jsx
@@ -1,19 +1,30 @@
 import { useEffect } from "react";
 import "./App.css";
 
+function DrawSection(){
+  return(
+  <>
+  <div className="draw">
+    <div className="bg-black w-[20vw] h-[15vw]"></div>
+    <div className="bg-black w-[20vw] h-[15vw]"></div>
+  </div>
+  </>)
+}
+
 function App() {
-  useEffect(() => {
-    fetch("http://localhost:3000/", {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((e) => console.log(e));
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/", {
+  //     method: "GET",
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data))
+  //     .catch((e) => console.log(e));
+  // }, []);
 
   return (
     <>
       <p className="bg-amber-300">Hi </p>
+      <DrawSection/>
     </>
   );
 }
